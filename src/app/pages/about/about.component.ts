4119e3845476e06e5879d9cb74da172c4f339879
unknown <yurchik2000@gmail.com>
2023-03-10 19:50:56 +0200

    add about page

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -7,4 +7,35 @@ import { Component } from '@angular/core';
 })
 export class AboutComponent {
 
+  aboutQuestion1: boolean = false;
+  aboutQuestion2: boolean = false;
+  aboutQuestion3: boolean = false;
+  aboutQuestion4: boolean = false;
+  
+  showDetails1(): void {
+    this.aboutQuestion1 = true;
+  }
+  hideDetails1(): void {
+    this.aboutQuestion1 = false;
+  }
+  showDetails2(): void {
+    this.aboutQuestion2 = true;
+  }
+  hideDetails2(): void {
+    this.aboutQuestion2 = false;
+  }
+  showDetails3(): void {
+    this.aboutQuestion3 = true;
+  }
+  hideDetails3(): void {
+    this.aboutQuestion3 = false;
+  }
+  showDetails4(): void {
+    this.aboutQuestion4 = true;
+  }
+  hideDetails4(): void {
+    this.aboutQuestion4 = false;
+  }
+
+
 }
